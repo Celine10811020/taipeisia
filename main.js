16b9bac65ae19cf705377762c4c122c62c2320b6
Celine10811020 <shinecloudhill@gmail.com>
2022-12-15 19:13:24 +0800

    Add from atom

--- main.js
+++ main.js
@@ -1,14 +1,142 @@
-$(document).ready(function(){
+//fontSize 初始值設定
+if(window.sessionStorage.getItem('fontSize') == 'big')
+{
+  var element = document.getElementById("story");
+  element.style.fontSize = "18px";
+  element.style.lineHeight = "2.2";
 
-  //背景更改
+}else if(window.sessionStorage.getItem('fontSize') == 'middle')
+{
+  var element = document.getElementById("story");
+  element.style.fontSize = "16px";
+  element.style.lineHeight = "2";
+  
+}else if(window.sessionStorage.getItem('fontSize') == 'small')
+{
+  var element = document.getElementById("story");
+  element.style.fontSize = "14px";
+  element.style.lineHeight = "1.8";
+}
+
+//background 初始值設定
+if(window.sessionStorage.getItem('background') == 'white')
+{
+  var element = document.getElementById("header");
+  element.style.background = "#DDDDDD";
+  element.style.color = "#000000";
+  element = document.getElementById("article");
+  element.style.background = "#FFFFFF";
+  element.style.color = "#000000";
+  element = document.getElementById("footer");
+  element.style.background = "#DDDDDD";
+  element.style.color = "#000000";
+  element = document.getElementById("lastChapter");
+  element.style.background = "#FFFFFF";
+  element.style.color = "#000000";
+  element = document.getElementById("content");
+  element.style.background = "#FFFFFF";
+  element.style.color = "#000000";
+  element = document.getElementById("nextChapter");
+  element.style.background = "#FFFFFF";
+  element.style.color = "#000000";
+
+}else if(window.sessionStorage.getItem('background') == 'green')
+{
+  var element = document.getElementById("header");
+  element.style.background = "#A9EEAD";
+  element.style.color = "#000000";
+  element = document.getElementById("article");
+  element.style.background = "#F3FDEC";
+  element.style.color = "#000000";
+  element = document.getElementById("footer");
+  element.style.background = "#A9EEAD";
+  element.style.color = "#000000";
+  element = document.getElementById("lastChapter");
+  element.style.background = "#F3FDEC";
+  element.style.color = "#000000";
+  element = document.getElementById("content");
+  element.style.background = "#F3FDEC";
+  element.style.color = "#000000";
+  element = document.getElementById("nextChapter");
+  element.style.background = "#F3FDEC";
+  element.style.color = "#000000";
+
+}else if(window.sessionStorage.getItem('background') == 'blue')
+{
+  var element = document.getElementById("header");
+  element.style.background = "#61AEEE";
+  element.style.color = "#000000";
+  element = document.getElementById("article");
+  element.style.background = "#E9F4FC";
+  element.style.color = "#000000";
+  element = document.getElementById("footer");
+  element.style.background = "#61AEEE";
+  element.style.color = "#000000";
+  element = document.getElementById("lastChapter");
+  element.style.background = "#E9F4FC";
+  element.style.color = "#000000";
+  element = document.getElementById("content");
+  element.style.background = "#E9F4FC";
+  element.style.color = "#000000";
+  element = document.getElementById("nextChapter");
+  element.style.background = "#E9F4FC";
+  element.style.color = "#000000";
+
+}else if(window.sessionStorage.getItem('background') == 'gray')
+{
+  var element = document.getElementById("header");
+  element.style.background = "#808080";
+  element.style.color = "#000000";
+  element = document.getElementById("article");
+  element.style.background = "#DDDDDD";
+  element.style.color = "#000000";
+  element = document.getElementById("footer");
+  element.style.background = "#808080";
+  element.style.color = "#000000";
+  element = document.getElementById("lastChapter");
+  element.style.background = "#DDDDDD";
+  element.style.color = "#000000";
+  element = document.getElementById("content");
+  element.style.background = "#DDDDDD";
+  element.style.color = "#000000";
+  element = document.getElementById("nextChapter");
+  element.style.background = "#DDDDDD";
+  element.style.color = "#000000";
+
+}else if(window.sessionStorage.getItem('background') == 'black')
+{
+  var element = document.getElementById("header");
+  element.style.background = "#000000";
+  element.style.color = "#DDDDDD";
+  element = document.getElementById("article");
+  element.style.background = "#2C313C";
+  element.style.color = "#DDDDDD";
+  element = document.getElementById("footer");
+  element.style.background = "#000000";
+  element.style.color = "#DDDDDD";
+  element = document.getElementById("lastChapter");
+  element.style.background = "#3A3F4B";
+  element.style.color = "#DDDDDD";
+  element = document.getElementById("content");
+  element.style.background = "#3A3F4B";
+  element.style.color = "#DDDDDD";
+  element = document.getElementById("nextChapter");
+  element.style.background = "#3A3F4B";
+  element.style.color = "#DDDDDD";
+}
+
+
+$(document).ready(function(){
+  //background 更改
   $('.white').on('click',function(e){
-    event.preventDefault();//取消預設行為
+    event.preventDefault();
     $('header').css({'background-color':'#DDDDDD','color':'#000000'});
     $('article').css({'background-color':'#FFFFFF','color':'#000000'});
     $('footer').css({'background-color':'#DDDDDD','color':'#000000'});
     $('.lastChapter').css({'background-color':'#FFFFFF','color':'#000000'});
     $('.content').css({'background-color':'#FFFFFF','color':'#000000'});
     $('.nextChapter').css({'background-color':'#FFFFFF','color':'#000000'});
+    window.sessionStorage.setItem("background", 'white');
   });
 
   $('.green').on('click',function(e){
@@ -19,6 +147,7 @@ $(document).ready(function(){
     $('.lastChapter').css({'background-color':'#F3FDEC','color':'#000000'});
     $('.content').css({'background-color':'#F3FDEC','color':'#000000'});
     $('.nextChapter').css({'background-color':'#F3FDEC','color':'#000000'});
+    window.sessionStorage.setItem("background", 'green');
   });
 
   $('.blue').on('click',function(e){
@@ -29,6 +158,7 @@ $(document).ready(function(){
     $('.lastChapter').css({'background-color':'#E9F4FC','color':'#000000'});
     $('.content').css({'background-color':'#E9F4FC','color':'#000000'});
     $('.nextChapter').css({'background-color':'#E9F4FC','color':'#000000'});
+    window.sessionStorage.setItem("background", 'blue');
   });
 
   $('.gray').on('click',function(e){
@@ -39,6 +169,7 @@ $(document).ready(function(){
     $('.lastChapter').css({'background-color':'#DDDDDD','color':'#000000'});
     $('.content').css({'background-color':'#DDDDDD','color':'#000000'});
     $('.nextChapter').css({'background-color':'#DDDDDD','color':'#000000'});
+    window.sessionStorage.setItem("background", 'gray');
   });
 
   $('.black').on('click',function(e){
@@ -46,30 +177,31 @@ $(document).ready(function(){
     $('header').css({'background-color':'#000000','color':'#DDDDDD'});
     $('article').css({'background-color':'#2C313C','color':'#DDDDDD'});
     $('footer').css({'background-color':'#000000','color':'#DDDDDD'});
-    $('.lastChapter').css({'background-color':'#2C313C','color':'#DDDDDD'});
-    $('.content').css({'background-color':'#2C313C','color':'#DDDDDD'});
-    $('.nextChapter').css({'background-color':'#2C313C','color':'#DDDDDD'});
+    $('.lastChapter').css({'background-color':'#3A3F4B','color':'#DDDDDD'});
+    $('.content').css({'background-color':'#3A3F4B','color':'#DDDDDD'});
+    $('.nextChapter').css({'background-color':'#3A3F4B','color':'#DDDDDD'});
+    window.sessionStorage.setItem("background", 'black');
   });
 
-  //文字大小更改
-  $('.bigBig').on('click',function(e){
-    event.preventDefault();
-    $('.story').css('font-size','20px');
-  });
-
+  //fontSize 更改
   $('.big').on('click',function(e){
     event.preventDefault();
     $('.story').css('font-size','18px');
+    $('.story').css('line-height','2.2');
+    window.sessionStorage.setItem("fontSize", 'big');
   });
 
   $('.middle').on('click',function(e){
     event.preventDefault();
     $('.story').css('font-size','16px');
+    $('.story').css('line-height','2');
+    window.sessionStorage.setItem("fontSize", 'middle');
   });
 
   $('.small').on('click',function(e){
     event.preventDefault();
     $('.story').css('font-size','14px');
+    $('.story').css('line-height','1.8');
+    window.sessionStorage.setItem("fontSize", 'small');
   });
-
 });
